5ce7142f18385b0c448df11795676bd677866f0a
geethika01987 <geethikaurimindi01@gmail.com>
2025-07-04 21:40:27 +0530

    javafundamentals

---
+++ java_fundamentals/src/java_fundamentals/Question2.java
@@ -0,0 +1,13 @@
+package java_fundamentals;
+import java.util.*;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Name");
+		String name=sc.nextLine();
+		System.out.println("Welcome "+name);
+		sc.close();
+	}
+}
---
+++ java_fundamentals/src/java_fundamentals/Question3.java
@@ -0,0 +1,16 @@
+package java_fundamentals;
+import java.util.*; 
+
+public class Question3 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Num1");
+		int num1=sc.nextInt();
+		System.out.println("Enter Num2");
+		int num2=sc.nextInt();
+		System.out.println("the sum of "+num1+" and "+num2+ " is "+num1+num2);
+		sc.close();
+		
+	}
+
+}
